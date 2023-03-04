31732ba27fa274b4a1aa8bf2e409d0bda9131d67
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 <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@github.com>
2023-03-04 22:14:22 +0530

    added project files

---
+++ src/com/pratik_humbe/phase_1/Main.java
@@ -0,0 +1,13 @@
+package com.pratik_humbe.phase_1;
+public class Main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = ".";
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
